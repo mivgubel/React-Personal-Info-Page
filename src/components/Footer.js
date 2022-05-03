5a465768941c366fcb2ba6f1243c03256a1acae2
Migcap <angelcap1990@gmail.com>
2022-05-03 12:01:17 -0500

    final website

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Footer() {
+	return (
+		<div className="footer">
+			<button className="face">
+				<i className="bi bi-twitter"></i>
+			</button>
+
+			<button>
+				<i className="bi bi-facebook"></i>
+			</button>
+
+			<button>
+				<i className="bi bi-instagram"></i>
+			</button>
+
+			<button className="git">
+				<i className="bi bi-github"></i>
+			</button>
+		</div>
+	);
+}
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+	return (
+		<div className="head">
+			<img src="https://i.imgur.com/kCo9zUZ.png" alt="Foto de Perfil" />
+		</div>
+	);
+}
